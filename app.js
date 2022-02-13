8a95da67f27d1f9a9c7f968606fe7d9907669426
Piyush <piyushpathak3004@gmail.com>
2022-02-13 12:41:22 +0530

    My first frontend + backend site

---
+++ app.js
@@ -0,0 +1,96 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const _ = require('lodash');
+const lowerCase = require('lodash.lowercase');
+
+const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+const app = express();
+
+
+// Load the full build.
+
+
+
+
+
+
+
+
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+
+let postArray = [];
+let post ;
+
+app.get("/",function(req,res){
+  
+
+    res.render("home",{txt:homeStartingContent,
+                       postArray:postArray 
+                       });
+    
+});
+
+app.get("/post/:post",function(req,res){
+
+  let name = req.params.post;
+  name = _.lowerCase(name);
+  let body;
+  let ok =  0;
+  for(let i=0;i<postArray.length;i++){
+    if(postArray[i].postTitle ===  name ){
+      body = postArray[i].postBody;
+      ok = 1;
+      break;
+    }
+  }
+
+  if(ok){
+    res.render("post",{title: name,
+                       body: body});
+  }
+
+});
+
+app.get("/about",function(req,res){
+  res.render("about",{aboutMe: aboutContent});
+});
+
+
+app.get("/contact",function(req,res){
+  res.render("contact",{contactDetail: contactContent });
+});
+
+
+app.get("/compose",function(req,res){
+  
+  res.render("compose");
+  
+  
+});
+
+app.post("/compose",function(req,res){
+  let title = req.body.postTitle;
+  let body = req.body.postBody;
+  post = {  
+            postTitle:title,
+            postBody:body
+        }
+  postArray.push(post);
+  res.redirect("/");
+
+});
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
